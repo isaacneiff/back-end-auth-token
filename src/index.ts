07bbb2f300e8a393fbcc00db532b512cfa799590
isaac neiff <isaacalves98017@gmail.com>
2025-08-11 15:27:43 -0300

    subindo o back-end

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 const prisma = new PrismaClient();
 
 
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("API funcionando 🚀");
 });
